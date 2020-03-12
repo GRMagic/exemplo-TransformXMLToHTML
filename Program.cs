3ff24ed000ab52147bda4086c3f8ee515a0c6a13
Gustavo Rossi Müller <Gustavo.Muller@AFM.local>
2020-03-12 09:38:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace Teste_XSLT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = TransformXMLToHTML(File.ReadAllText(@"xmls\dados.xml"), File.ReadAllText(@"xmls\estilos.xml"));
+            string destino = @"xmls\resultado.html";
+            File.WriteAllText(destino, html);
+            System.Diagnostics.Process.Start(destino);
+        }
+
+        public static string TransformXMLToHTML(string inputXml, string xsltString)
+        {
+            XslCompiledTransform transform = new XslCompiledTransform();
+            using (XmlReader reader = XmlReader.Create(new StringReader(xsltString)))
+                transform.Load(reader);
+
+            StringWriter results = new StringWriter();
+            using (XmlReader reader = XmlReader.Create(new StringReader(inputXml)))
+                transform.Transform(reader, null, results);
+            
+            return results.ToString();
+        }
+    }
+}
